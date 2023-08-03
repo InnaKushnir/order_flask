9b8eeb7f3571dbb1bc12144591382985cd6ef410
Inna Kushnir <innakushnir63@gmail.com>
2023-08-03 11:01:49 +0300

    Solution

--- celery_.py
+++ celery_.py
@@ -7,7 +7,7 @@ broker_url = "redis://redis:6379/0"
 app = Celery("tasks", broker=broker_url)
 
 app.conf.update(
-    result_backend=os.getenv('MYSQL_RESULT_BACKEND'),
+    result_backend="redis://localhost:6379/0",
     task_serializer="json",
     result_serializer="json",
     accept_content=["json"],
